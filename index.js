44b7a9b6af6bfabd6e383424fa86e263d11afce5
LukasD <lukas.dvarionis@gmail.com>
2022-10-03 14:17:04 +0300

    8.7: Updating the birth year of an author Implemented editAuthor mutation

--- index.js
+++ index.js
@@ -123,6 +123,10 @@ const typeDefs = gql`
       author: String!
       genres: [String]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -158,6 +162,14 @@ const resolvers = {
           authors.push(author)
         }
       return book
+    },
+    editAuthor: (root, args) => {
+      const authorToEdit = authors.find(author => author.name === args.name)
+      if(!authorToEdit)
+        return null
+      authorToEdit.born = args.setBornTo
+      authors = authors.map(author => author.name === args.name? authorToEdit: author)
+      return authorToEdit
     }
   }
 }
